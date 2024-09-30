ad3753efb47b1aa166130780bed90c64ce658538
JHermosillaD <hermosilladiaz@gmail.com>
2024-09-30 17:31:43 -0600

    Update lenet.py

--- LeNet5/lenet.py
+++ LeNet5/lenet.py
@@ -17,13 +17,13 @@ class LeNet5(nn.Module):
         self.train_set = datasets.MNIST(root='./',
                                         train=True,
                                         download=True,
-                                        transform=transforms.Compose([transforms.Resize((32,32)),
-                                                                    transforms.ToTensor()]))
+                                        transform=transforms.Compose([transforms.ToTensor(),
+                                                                      transforms.Normalize(mean = (0.1307,), std = (0.3081,))]))
         self.test_set  = datasets.MNIST(root='./',
                                         train=False,
                                         download=True,
-                                        transform=transforms.Compose([transforms.Resize((32,32)),
-                                                                    transforms.ToTensor()]))
+                                        transform=transforms.Compose([transforms.ToTensor(),
+                                                                      transforms.Normalize(mean = (0.1307,), std = (0.3081,))]))
         self.train_loader = torch.utils.data.DataLoader(dataset=self.train_set,
                                                         batch_size=self.batch_size,
                                                         shuffle=True)        
@@ -36,17 +36,18 @@ class LeNet5(nn.Module):
                                                 nn.MaxPool2d(kernel_size = 2, stride = 2))
         self.convolution_layer2 = nn.Sequential(nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
                                                 nn.Tanh(),
-                                                nn.MaxPool2d(kernel_size = 2, stride = 2))        
-        self.convolution_layer3 = nn.Sequential(nn.Conv2d(16, 120, kernel_size=5, stride=1, padding=0),
-                                                nn.Tanh())        
-        self.fully_connected1 = nn.Linear(120, 64)
-        self.fully_connected2 = nn.Linear(64, 10)
+                                                nn.MaxPool2d(kernel_size = 2, stride = 2))    
+        self.convolution_layer3 = nn.Sequential(nn.Conv2d(16, 120, kernel_size=4, stride=1, padding=0))    
+        self.fully_connected1 = nn.Sequential(nn.Flatten(),
+                                              nn.Linear(120*1*1, 64),
+                                              nn.Tanh())           
+        self.fully_connected2 = nn.Sequential(nn.Linear(64, 10),
+                                              nn.Tanh())                  
 
     def forward(self, x):
             out = self.convolution_layer1(x)
             out = self.convolution_layer2(out)
             out = self.convolution_layer3(out)
-            out = out.reshape(out.size(0), -1)
             out = self.fully_connected1(out)
             out = self.fully_connected2(out)
             return out
@@ -54,48 +55,57 @@ class LeNet5(nn.Module):
 class Learning_classrom():
     def __init__(self, model):
         self.model = model
-        self.epochs = 5
+        self.epochs = 10
         self.device = torch.device(dev)
         self.loss_func = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
 
     def train(self):
         x_train = []
         y_train = []
-        iteration = 0
         for epoch in range(self.epochs):
+            running_loss = 0.0
+            count = 0
             for i, (images, labels) in enumerate(self.model.train_loader):     
                 images = images.to(self.device)
                 labels = labels.to(self.device)          
+                self.optimizer.zero_grad()
                 outputs = self.model(images)
                 loss = self.loss_func(outputs, labels)
-                self.optimizer.zero_grad()
                 loss.backward()
-                self.optimizer.step()     
-                x_train.append(iteration)
-                y_train.append(loss.item())
-                iteration += 1
+                self.optimizer.step()
+                with torch.no_grad():
+                    running_loss += loss.item()  
+                if i % 100 == 99:
+                    count += 1
+                    if (count == 9):
+                        print('epoch: %d loss: %.3f' %(epoch + 1, running_loss/100))
+                        x_train.append(epoch)
+                        y_train.append(running_loss)
+                    running_loss = 0.0
         plt.plot(np.array(x_train), np.array(y_train))
-        plt.show()
+        plt.show()       
 
     def test(self):
         x_test = []
         y_test = []
         iteration = 0
         with torch.no_grad():
-            for images, labels in self.model.test_loader:
-                    images = images.to(self.device)
-                    labels = labels.to(self.device)
-                    outputs = self.model(images)
-                    loss = self.loss_func(outputs, labels)
-                    x_test.append(iteration)
-                    y_test.append(loss.item())
-                    iteration += 1
-        plt.plot(np.array(x_test), np.array(y_test))
-        plt.show()
+            for epoch in range(self.epochs):
+                for images, labels in self.model.test_loader:
+                        images = images.to(self.device)
+                        labels = labels.to(self.device)
+                        outputs = self.model(images)
+                        loss = self.loss_func(outputs, labels)
+                x_test.append(iteration)
+                y_test.append(loss.item())
+                iteration += 1
+            plt.plot(np.array(x_test), np.array(y_test))
+            plt.show()
 
 def main():    
     model = LeNet5()
+    print(model)
     model.to(torch.device(dev))
     learning = Learning_classrom(model)
     learning.train()
