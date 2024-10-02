9a187cff24ae156249d0fcfa89fb9d9dfc7e0935
JHermosillaD <hermosilladiaz@gmail.com>
2024-10-01 22:37:29 -0600

    Update lenet.py

--- LeNet5/lenet.py
+++ LeNet5/lenet.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from torchmetrics import Accuracy
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,13 +18,11 @@ class LeNet5(nn.Module):
         self.train_set = datasets.MNIST(root='./',
                                         train=True,
                                         download=True,
-                                        transform=transforms.Compose([transforms.ToTensor(),
-                                                                      transforms.Normalize(mean = (0.1307,), std = (0.3081,))]))
+                                        transform=transforms.Compose([transforms.ToTensor()]))
         self.test_set  = datasets.MNIST(root='./',
                                         train=False,
                                         download=True,
-                                        transform=transforms.Compose([transforms.ToTensor(),
-                                                                      transforms.Normalize(mean = (0.1307,), std = (0.3081,))]))
+                                        transform=transforms.Compose([transforms.ToTensor()]))
         self.train_loader = torch.utils.data.DataLoader(dataset=self.train_set,
                                                         batch_size=self.batch_size,
                                                         shuffle=True)        
@@ -51,70 +50,72 @@ class LeNet5(nn.Module):
             out = self.fully_connected2(out)
             return out
     
-class Learning_classrom():
+class Learning_class():
     def __init__(self, model):
-        self.model = model
         self.epochs = 10
         self.device = torch.device(dev)
+        self.model = model.to(self.device)
         self.loss_func = nn.CrossEntropyLoss()
+        self.accuracy = Accuracy(task='multiclass', num_classes=10)
+        self.accuracy = self.accuracy.to(dev)
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+        self.train_loss_hist = []
+        self.train_acc_hist = []
+        self.test_loss_hist = []
+        self.test_acc_hist = []
+        self.epochs_hist = []
 
-    def train(self):
-        x_train = []
-        y_train = []
+    def train(self, history = False):
         for epoch in range(self.epochs):
-            running_loss = 0.0
-            count = 0
-            for i, (images, labels) in enumerate(self.model.train_loader):     
+            train_loss, train_acc = 0.0, 0.0
+            for images, labels in self.model.train_loader:
                 images = images.to(self.device)
-                labels = labels.to(self.device)          
-                self.optimizer.zero_grad()
+                labels = labels.to(self.device)
+                self.model.train()
                 outputs = self.model(images)
                 loss = self.loss_func(outputs, labels)
+                with torch.no_grad():
+                    train_loss += loss.item()
+                acc = self.accuracy(outputs, labels)
+                train_acc += acc
+                self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                with torch.no_grad():
-                    running_loss += loss.item()  
-                if i % 100 == 99:
-                    count += 1
-                    if (count == 9):
-                        print('epoch: %d trainning loss: %.3f' %(epoch + 1, running_loss/100))
-                        x_train.append(epoch)
-                        y_train.append(running_loss)
-                    running_loss = 0.0
-        plt.plot(np.array(x_train), np.array(y_train))
-        plt.show()       
+            train_loss /= len(self.model.train_loader)
+            train_acc /= len(self.model.train_loader)
 
-    def test(self):
-        x_test = []
-        y_test = []
-        with torch.no_grad():
-            for epoch in range(self.epochs):
-                running_loss = 0.0
-                count = 0
-                for i, (images, labels) in enumerate(self.model.test_loader):     
+            test_loss, test_acc = 0.0, 0.0
+            self.model.eval()
+            with torch.inference_mode():
+                for images, labels in self.model.test_loader:
                     images = images.to(self.device)
                     labels = labels.to(self.device)
                     outputs = self.model(images)
-                    loss = self.loss_func(outputs, labels)
-                    running_loss += loss.item() 
-                    if i % 100 == 99:
-                        count += 1
-                        if (count == 9):
-                            print('epoch: %d testing loss: %.3f' %(epoch + 1, running_loss/100))
-                            x_test.append(epoch)
-                            y_test.append(running_loss)
-                        running_loss = 0.0
-            plt.plot(np.array(x_test), np.array(y_test))
-            plt.show()
+                    loss = self.loss_func(outputs,labels)
+                    acc = self.accuracy(outputs,labels)
+                    with torch.no_grad():
+                        test_loss += loss.item()
+                        test_acc += acc
+                test_loss /= len(self.model.test_loader)
+                test_acc /= len(self.model.test_loader)
+
+            print(f"Epoch: {epoch+1} Train loss: {train_loss: .5f} Train acc: {train_acc: .5f} Val loss: {test_loss: .5f} Val acc: {test_acc: .5f}")
+            if (history):
+                self.train_loss_hist.append(train_loss)
+                self.train_acc_hist.append(train_acc)
+                self.test_loss_hist.append(test_loss)
+                self.test_acc_hist.append(test_acc)
+                self.epochs_hist.append(epoch+1)
+
+    def plot_history(self):
+        plt.plot(np.array(self.train_loss_hist), np.array(self.epochs_hist))
+        plt.show()       
 
 def main():    
     model = LeNet5()
-    print(model)
-    model.to(torch.device(dev))
-    learning = Learning_classrom(model)
-    learning.train()
-    learning.test()
+    learning_model = Learning_class(model)
+    learning_model.train(history=True)
+    learning_model.plot_history()
 
 if __name__ == '__main__':
     main()
